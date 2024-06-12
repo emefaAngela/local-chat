e0e81d71906c7c48bee10580cf3613a93d3af072
emefaAngela <gelamengor@gmail.com>
2024-06-12 06:28:58 +0000

    styling log in page

--- src/login.js
+++ src/login.js
@@ -9,11 +9,12 @@ const Login = () => {
         sessionStorage.setItem('username', name);
     }
     return ( 
-        <div className="h-screen w-screen flex m-auto justify-center items-center bg-slate-700">
+        <div className="h-screen w-screen flex m-auto justify-center items-center bg-[#1E243F]">
 
-            <div className="w-72 h-64 rounded-md shadow-md flex flex-col m-auto justify-center space-y-12 bg-slate-300">
-                <input type="text" value={name } placeholder="Your name" onChange={handleSetName} className="w-48 mx-12 h-10 rounded-full bg-white outline-none px-2" />
-                <button className="bg-slate-800 text-white py-1 px-2 rounded-full m-auto w-32"><a onClick={handleLogin} href='/chat'>Start chatting</a></button>
+            <div className="w-72 h-64 rounded-md shadow-md flex flex-col m-auto justify-center items-center space-y-12 bg-[#E5E5E5]">
+                <div className="">What should we call you?</div>
+                <input type="text" value={name } placeholder="username" onChange={handleSetName} className="w-48 mx-12  rounded-lg bg-white text-[#9CA3AF] outline-none px-4 py-3" />
+                <button className="bg-[#1F3C88] text-white py-3 px-4 rounded-lg m-auto w-48"><a onClick={handleLogin} href='/chat'>Start chatting</a></button>
                
             </div>
         </div>
